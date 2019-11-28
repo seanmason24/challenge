01f9abdd0a723f12ec6fda45b88c878a7923559b
Your Name <you@example.com>
2019-11-28 00:33:06 +0000

    first commit

---
+++ imageblur.rb
@@ -0,0 +1,24 @@
+class Image
+  attr_accessor :image
+
+  def initialize (image)
+  @image = image
+  end
+
+  def output_image 
+  @image.each do |arr|
+  puts arr.join
+
+ end
+end
+
+end
+
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
